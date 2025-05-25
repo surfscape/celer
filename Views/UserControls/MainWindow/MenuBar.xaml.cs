6def7601c5ac157e30084b1d7d906c5f1c7e3930
ReduxFlakes <tiagompca@proton.me>
2025-05-25 18:55:59 +0100

    ref: menubar viewmodel

--- Views/UserControls/MainWindow/MenuBar.xaml.cs
+++ Views/UserControls/MainWindow/MenuBar.xaml.cs
@@ -1,24 +1,28 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Navigation;
 using Celer.Views.Windows;
 using Celer.Views.Windows.Utils;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Celer.Views.UserControls.MainWindow
 {
     /// <summary>
     /// Interaction logic for MenuBar.xaml used on the MainWindow.
     /// </summary>
-    [INotifyPropertyChanged]
     public partial class MenuBar : UserControl
     {
-        [ObservableProperty]
-        private bool schoolFeatureCheckStatus;
         public MenuBar()
         {
             InitializeComponent();
-            EnableSchoolFeatureCheckbox.DataContext = this;
-            SchoolFeatureCheckStatus = Properties.MainConfiguration.Default.SchoolFeature;
+            EnableSchoolFeatureCheckbox.IsEnabled = Properties.MainConfiguration.Default.SchoolFeature;
+        }
+
+        private void EnableSchoolFeatureCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.MainConfiguration.Default.SchoolFeature = EnableSchoolFeatureCheckbox.IsChecked;
+            Properties.MainConfiguration.Default.Save();
         }
 
         private void OpenAmbientChecker_Click(object sender, RoutedEventArgs e)
@@ -40,14 +44,16 @@ namespace Celer.Views.UserControls.MainWindow
         /// Helper function that opens a specific window, prohibits opening another instance of it and has the ability to bring it to the foreground if already opened.
         /// </summary>
         /// <param name="window">Object of the desired window to open</param>
-        private void OpenWindow<T>() where T : Window, new()
+        private static void OpenWindow<T>() where T : Window, new()
         {
                 Window? window = Application.Current.Windows.OfType<T>().FirstOrDefault();
                 if (window == null || !window.IsVisible)
                 {
-                    window = new T();
-                    window.Owner = Application.Current.MainWindow;
-                    window.ShowDialog();
+                window = new T
+                {
+                    Owner = Application.Current.MainWindow
+                };
+                window.ShowDialog();
                     window.Closed += (s, args) => window = null;
                 }
                 else
