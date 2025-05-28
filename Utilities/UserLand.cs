b5f9c190f5274674fdaeaf8823602f38b1d8e22a
ReduxFlakes <tiagompca@proton.me>
2025-05-28 22:25:05 +0100

    add: UserLand class for methods related to user actions

---
+++ Utilities/UserLand.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Windows;
+
+namespace Celer.Utilities
+{
+    public class UserLand
+    {
+        public void OpenLink(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open link: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public static bool IsInternetAvailable()
+        {
+            try
+            {
+                using Ping ping = new();
+                PingReply reply = ping.Send("9.9.9.9", 2000); // TODO: add a configuration option for the ping server if the default one is censored in some countries
+                return reply.Status == IPStatus.Success;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
