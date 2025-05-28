5db2399c26b0569bef46f6ebd1fe10bedaa65ae7
ReduxFlakes <tiagompca@proton.me>
2025-05-28 23:20:01 +0100

    new revision of CleanEngine
    - Improved user log
    - Better handling for null references
    - Checking for item selection
    - Reimplemented signature loading
    - Makes the use of AppGlobal for the EnableCleanEngine state

--- ViewModels/CleanEngine.cs
+++ ViewModels/CleanEngine.cs
@@ -1,4 +1,5 @@
-﻿using Celer.Services;
+﻿using Celer.Models;
+using Celer.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
@@ -14,9 +15,8 @@ namespace Celer.ViewModels
         [ObservableProperty] private ObservableCollection<CleanupCategory> categories = [];
         [ObservableProperty] private string logText = string.Empty;
         [ObservableProperty] private double totalFreedText = 0;
-        [ObservableProperty] private bool canClean = false;
+        [ObservableProperty] private bool canClean = AppGlobals.EnableCleanEngine;
         [ObservableProperty] private object? selectedItem;
-        
 
         public CleanEngine() { 
             LoadJson();
@@ -24,7 +24,27 @@ namespace Celer.ViewModels
 
         private void LoadJson()
         {
+            const string path = "signatures.json";
             
+            if (!File.Exists(path))
+            {
+                LogText = "Ficheiro de assinaturas de limpeza não encontrado, tenta atualizar as assinaturas através do menu > Ferramentas > Verificar Atualizações\n";
+                AppGlobals.EnableCleanEngine = false;
+            }
+            try
+            {
+                LogText = "A carregar assinaturas...\n";
+                var json = File.ReadAllText(path);
+                ParseJson(json);
+                AppGlobals.EnableCleanEngine = true;
+                LogText += "Assinaturas carregadas com sucesso!\n";
+            }
+            catch(Exception e)
+            {
+                LogText = $"Ocorreu um erro a carregar as assinaturas: {e.Message}\n";
+                AppGlobals.EnableCleanEngine = false;
+
+            }
         }
 
         private void ParseJson(string json)
@@ -35,13 +55,19 @@ namespace Celer.ViewModels
                 var items = new ObservableCollection<CleanupItem>();
                 foreach (var item in cat.Value.EnumerateArray())
                 {
-                    items.Add(new CleanupItem
+                    var name = item.GetProperty("name").GetString();
+                    var path = item.GetProperty("path").GetString();
+
+                    if (name != null && path != null)
                     {
-                        Name = item.GetProperty("name").GetString(),
-                        Path = item.GetProperty("path").GetString(),
-                        RequiredProcesses = [.. item.GetProperty("requiredProcesses").EnumerateArray().Select(p => p.GetString())],
-                        IsChecked = false
-                    });
+                        items.Add(new CleanupItem
+                        {
+                            Name = name,
+                            Path = path,
+                            RequiredProcesses = [.. item.GetProperty("requiredProcesses").EnumerateArray().Select(p => p.GetString()!)],
+                            IsChecked = false
+                        });
+                    }
                 }
                 Categories.Add(new CleanupCategory { Name = cat.Name, Items = items });
             }
@@ -51,105 +77,114 @@ namespace Celer.ViewModels
         [RelayCommand]
         private async Task CleanAsync()
         {
-            LogText += ("A iniciar Celer Cleaning Engine...");
             TotalFreedText = 0;
 
-            long totalFreed = 0;
-            var runningProcs = Process.GetProcesses().Select(p => p.ProcessName.ToLower()).ToHashSet();
+            var selectedItems = Categories
+                .SelectMany(c => c.Items)
+                .Where(i => i.IsChecked)
+                .ToList();
+
+            if (selectedItems.Count == 0)
+            {
+                LogText += "É necessário selecionar pelo menos um item para iniciar a limpeza.\n";
+                CanClean = true;
+                return;
+            }
+
+            var runningProcs = Process.GetProcesses()
+                .Select(p => p.ProcessName.ToLower())
+                .ToHashSet();
+
             var toClose = new HashSet<string>();
 
-            foreach (var category in Categories)
+            foreach (var item in selectedItems)
             {
-                foreach (var item in category.Items.Where(i => i.IsChecked))
+                foreach (var proc in item.RequiredProcesses)
                 {
-                    foreach (var proc in item.RequiredProcesses)
-                    {
-                        if (runningProcs.Contains(Path.GetFileNameWithoutExtension(proc).ToLower()))
-                            toClose.Add(proc);
-                    }
+                    if (runningProcs.Contains(Path.GetFileNameWithoutExtension(proc).ToLower()))
+                        toClose.Add(proc);
                 }
             }
 
             if (toClose.Count > 0)
             {
-                LogText = "Please close the following applications:\n" + string.Join("\n", toClose);
+                LogText += "É necessário fechar as seguintes aplicações para continuar:\n" + string.Join("\n", toClose) + "\n";
                 return;
             }
 
             StringBuilder log = new();
+            long totalFreed = 0;
 
             await Task.Run(() =>
             {
                 CanClean = false;
-                foreach (var category in Categories)
+                LogText += "A iniciar Celer Cleaning Engine...\n";
+                foreach (var item in selectedItems)
                 {
-                    foreach (var item in category.Items.Where(i => i.IsChecked))
-                    {
-                        string resolvedPath = Environment.ExpandEnvironmentVariables(item.Path);
-                        long freed = 0;
-                        try
-                        {
-                            if (Directory.Exists(resolvedPath))
-                            {
-                                var dir = new DirectoryInfo(resolvedPath);
-                                foreach (var file in dir.GetFiles("*", SearchOption.AllDirectories))
-                                {
-                                    try
-                                    {
-                                        freed += file.Length;
-                                        file.Attributes = FileAttributes.Normal;
-                                        file.Delete();
-                                        log.AppendLine($"Removido o ficheiro: {file.FullName}\n");
-                                        LogText += log.ToString();
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        log.AppendLine($"Falha ao apagar ficheiro: {file.FullName}: {ex.Message}\n");
-                                        LogText += log.ToString();
-                                    }
-                                }
+                    string resolvedPath = Environment.ExpandEnvironmentVariables(item.Path);
+                    long freed = 0;
 
+                    try
+                    {
+                        if (Directory.Exists(resolvedPath))
+                        {
+                            var dir = new DirectoryInfo(resolvedPath);
+                            foreach (var file in dir.GetFiles("*", SearchOption.AllDirectories))
+                            {
                                 try
                                 {
-                                    dir.Delete(true);
+                                    freed += file.Length;
+                                    file.Attributes = FileAttributes.Normal;
+                                    file.Delete();
+                                    log.AppendLine($"Removido o ficheiro: {file.FullName}");
                                 }
                                 catch (Exception ex)
                                 {
-                                    log.AppendLine($"Falha ao apagar pasta: {resolvedPath}: {ex.Message}");
-                                    LogText += log.ToString();
+                                    log.AppendLine($"Falha ao apagar ficheiro: {file.FullName}: {ex.Message}");
                                 }
                             }
-                            else if (File.Exists(resolvedPath))
-                            {
-                                var fileInfo = new FileInfo(resolvedPath)
-                                {
-                                    Attributes = FileAttributes.Normal
-                                };
-                                freed += fileInfo.Length;
-                                fileInfo.Delete();
-                            }
 
-                            totalFreed += freed;
-                            log.AppendLine($"Removido {resolvedPath} ({freed / 1024} KB)");
-                            LogText += log.ToString();
+                            try
+                            {
+                                dir.Delete(true);
+                            }
+                            catch (Exception ex)
+                            {
+                                log.AppendLine($"Falha ao apagar pasta: {resolvedPath}: {ex.Message}");
+                            }
                         }
-                        catch (Exception ex)
+                        else if (File.Exists(resolvedPath))
                         {
-                            log.AppendLine($"Falha ao apagar {resolvedPath}: {ex.Message}");
-                            LogText += log.ToString();
+                            var fileInfo = new FileInfo(resolvedPath)
+                            {
+                                Attributes = FileAttributes.Normal
+                            };
+                            freed += fileInfo.Length;
+                            fileInfo.Delete();
                         }
+
+                        log.AppendLine($"Removido {resolvedPath} ({freed / 1024} KB)");
                     }
+                    catch (Exception ex)
+                    {
+                        log.AppendLine($"Falha ao apagar {resolvedPath}: {ex.Message}");
+                    }
+
+                    Interlocked.Add(ref totalFreed, freed);
                 }
             });
+
+            LogText += log.ToString();
             TotalFreedText = (long)(totalFreed / 1024f / 1024f);
         }
+
 
 
         public partial class CleanupItem : ObservableObject
         {
             public string Name { get; set; }
             public string Path { get; set; }
-            public List<string> RequiredProcesses { get; set; }
+            public List<string>? RequiredProcesses { get; set; }
 
             [ObservableProperty]
             private bool isChecked;
