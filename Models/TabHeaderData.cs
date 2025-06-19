2926f357e0307e93ac0eba795c0a2ee6abd06a2f
ReduxFlakes <tiagompca@proton.me>
2025-06-19 23:28:23 +0100

    made tabheaders consistent

---
+++ Models/TabHeaderData.cs
@@ -0,0 +1,8 @@
+﻿namespace Celer.Models
+{
+    public class TabHeaderData
+    {
+        public string? Title { get; set; }
+        public MahApps.Metro.IconPacks.PackIconLucideKind Icon { get; set; }
+    }
+}
