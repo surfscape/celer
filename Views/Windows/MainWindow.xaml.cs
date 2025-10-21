8b28e85598fcbfa46a5b3b3a3e0542d079db9cf5
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2025-10-22 00:32:34 +0100

    removed news dialog

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Celer.ViewModels;
-using Celer.Views.Windows.Utils;
 using System.Windows;
 
 namespace Celer.Views.Windows;
@@ -13,12 +12,5 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = viewModel;
-        Loaded += Window_Loaded;
-    }
-
-    private void Window_Loaded(object sender, RoutedEventArgs e)
-    {
-        var newsWindow = new News();
-        newsWindow.Show();
     }
 }
