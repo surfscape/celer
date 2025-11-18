8147cf886d3f01e97e4135f96fefd616f5eb19a4
ReduxFlakes <tiagompca@proton.me>
2025-11-18 22:12:05 +0000

    fix: white space on the english language resource

--- Resources/i18n/Strings.Designer.cs
+++ Resources/i18n/Strings.Designer.cs
@@ -19,7 +19,7 @@ namespace Celer.Resources.i18n {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Strings {
@@ -133,7 +133,7 @@ namespace Celer.Resources.i18n {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to I&apos;ve read and accept the Terms of Usage and .
+        ///   Looks up a localized string similar to I&apos;ve read and accept the Terms of Usage and.
         /// </summary>
         public static string Onboarding_LegalCheckboxLabel {
             get {
