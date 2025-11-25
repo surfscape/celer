37585e96c5403f4d0942db9fb50fac36a81cf975
ReduxFlakes <tiagompca@proton.me>
2025-11-25 01:45:10 +0000

    chore: formatting

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -8,8 +8,6 @@ namespace Celer.Views.Windows;
 /// </summary>
 public partial class MainWindow : Window
 {
-
-
     public MainWindow(MainWindowViewModel viewModel)
     {
         InitializeComponent();
