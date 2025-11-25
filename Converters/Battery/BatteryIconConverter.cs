124993a00cb095e3fdc7864e9e12b75ad9ab73a7
ReduxFlakes <tiagompca@proton.me>
2025-11-25 01:44:30 +0000

    feat: converter that returns the battery icon corresponding to the battery percentage

---
+++ Converters/Battery/BatteryIconConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Celer.Converters.Battery
+{
+    public class BatteryIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is int batteryLevel) { 
+                if(batteryLevel >= 70)
+                    return MahApps.Metro.IconPacks.PackIconLucideKind.BatteryFull;
+                if(batteryLevel >= 50)
+                    return MahApps.Metro.IconPacks.PackIconLucideKind.BatteryMedium;
+                if(batteryLevel <= 30)
+                    return MahApps.Metro.IconPacks.PackIconLucideKind.BatteryLow;
+            }
+            return MahApps.Metro.IconPacks.PackIconLucideKind.BatteryWarning;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture
+        ) => throw new NotSupportedException();
+    }
+}
