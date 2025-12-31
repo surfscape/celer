c3a03dccba0c2dc0d66a14d5a44a3ba7f2901866
ReduxFlakes <tiagompca@proton.me>
2025-12-31 02:08:44 +0000

    refactor: MemoryManagement module cleanup

--- Models/SystemInfo/MemoryInfo.cs
+++ Models/SystemInfo/MemoryInfo.cs
@@ -6,25 +6,25 @@
         public double TotalMemoryMB { get; set; }
         public double MemoryUsagePercentage =>
             TotalMemoryMB == 0 ? 0 : (UsedMemoryMB / TotalMemoryMB) * 100;
-        public float VirtualUsedMB { get; set; }
+        public double VirtualUsedMB { get; set; }
         public double VirtualTotalMB { get; set; }
         public double VirtualUsagePercentage =>
             VirtualTotalMB == 0 ? 0 : (VirtualUsedMB / VirtualTotalMB) * 100;
         public float? SpeedMHz { get; set; }
-        public List<RamSlotInfo> Slots { get; set; } = new();
+        public List<RamSlotInfo> Slots { get; set; } = [];
     }
 
     public class RamSlotInfo
     {
-        public string SlotNumber { get; set; }
-        public bool IsOccupied { get; set; }
-        public string Manufacturer { get; set; }
-        public string Model { get; set; }
+        public string SlotNumber { get; set; } = String.Empty;
+        public bool IsOccupied { get; set; } = false;
+        public string Manufacturer { get; set; } = String.Empty;
+        public string Model { get; set; } = String.Empty;
         public int SizeMB { get; set; }
-        public string MemoryType { get; set; }
-        public string FormFactor { get; set; }
-        public string SerialNumber { get; set; }
-        public string BankLabel { get; set; }
-        public string DeviceLocator { get; set; }
+        public string MemoryType { get; set; } = String.Empty;
+        public string FormFactor { get; set; } = String.Empty;
+        public string SerialNumber { get; set; } = String.Empty;
+        public string BankLabel { get; set; } = String.Empty;
+        public string DeviceLocator { get; set; } = String.Empty;
     }
 }
--- ViewModels/OptimizationVM/MemoryViewModel.cs
+++ ViewModels/OptimizationVM/MemoryViewModel.cs
@@ -1,35 +1,35 @@
 ﻿using Celer.Models.SystemInfo;
 using Celer.Services.Memory;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using Ookii.Dialogs.Wpf;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Celer.ViewModels.OptimizationVM
 {
     public partial class MemoryViewModel : ObservableObject
     {
         private readonly MemoryMonitorService _monitorService = new();
-        private readonly Timer _updateTimer;
+        private readonly DispatcherTimer _updateTimer = new()
+        {
+            Interval = TimeSpan.FromSeconds(2),
+        };
 
         [ObservableProperty]
-        private MemoryInfo memory;
+        private MemoryInfo? memory;
 
-        public ObservableCollection<RamSlotInfo> Slots { get; } = new();
+        public ObservableCollection<RamSlotInfo> Slots { get; } = [];
 
         public MemoryViewModel()
         {
             UpdateMemoryInfo(false);
-            _updateTimer = new Timer(_ => UpdateMemoryInfo(true), null, 0, 1000);
+            _updateTimer.Tick += (_, _) => UpdateMemoryInfo(true);
         }
 
         private void UpdateMemoryInfo(bool continous)
         {
             var mem = _monitorService.GetMemoryInfo();
             Memory = mem;
-
             if (!continous)
             {
                 Application.Current.Dispatcher.Invoke(() =>
@@ -39,87 +39,6 @@ namespace Celer.ViewModels.OptimizationVM
                         Slots.Add(slot);
                 });
             }
-        }
-
-        [RelayCommand]
-        private void CleanCache()
-        {
-            ShowCleanCacheDialogAsync();
-        }
-
-        private void ShowCleanCacheDialogAsync()
-        {
-            if (TaskDialog.OSSupportsTaskDialogs)
-            {
-                using TaskDialog dialog = new();
-                dialog.WindowTitle = "Celer Memory Service";
-                dialog.MainInstruction = "Limpar cache da memória";
-                dialog.Content =
-                    "Esta operação irá solicitar ao Windows que execute tarefas pendentes para libertar memória adicional. Durante este processo, poderá ocorrer alguma instabilidade no sistema, pelo que não é recomendado utilizar o computador enquanto a operação decorre.";
-                dialog.Footer =
-                    "Esta mensagem é apresentada pelo serviço de memória do Celer. Para mais informações sobre o seu funcionamento, consulte: <a href=\"https://surfscape.github.io/blueprint/celer/services/memory\">surfscape.github.io/blueprint/celer/services/memory</a>.";
-                dialog.FooterIcon = TaskDialogIcon.Information;
-                dialog.EnableHyperlinks = true;
-
-                var okButton = new TaskDialogButton(ButtonType.Ok);
-                var cancelButton = new TaskDialogButton(ButtonType.Cancel);
-                dialog.Buttons.Add(okButton);
-                dialog.Buttons.Add(cancelButton);
-
-                dialog.HyperlinkClicked += TaskDialog_HyperLinkClicked;
-
-                TaskDialogButton result = dialog.ShowDialog();
-
-                if (result == okButton)
-                    CleanCacheAction();
-            }
-            else
-            {
-                MessageBox.Show(
-                    "O sistema não suporta TaskDialog, verifique se está a executar o Celer no Windows 10/11 e que tem a última versão de .NET 9 instalada",
-                    "TaskDialog Failure"
-                );
-            }
-        }
-
-        private static void CleanCacheAction()
-        {
-            var proc = Process.Start(new ProcessStartInfo
-            {
-                FileName = "rundll32.exe",
-                Arguments = "advapi32.dll,ProcessIdleTasks",
-                CreateNoWindow = true,
-                UseShellExecute = false
-            });
-            if (proc is not null)
-            {
-                proc.Start();
-                proc.WaitForExit();
-                if (proc.HasExited)
-                {
-                    MessageBox.Show(
-                        "A limpeza do cache foi iniciada. O sistema pode demorar algum tempo a libertar memória adicional. Por favor, aguarde.",
-                        "Limpeza de Cache",
-                        MessageBoxButton.OK,
-                        MessageBoxImage.Information
-                    );
-                }
-            }
-            MessageBox.Show("Erro ao inicar processo", "Limpeza de Cache");
-        }
-
-        private void TaskDialog_HyperLinkClicked(object? sender, HyperlinkClickedEventArgs e)
-        {
-            if (e.Href != null)
-            {
-                Process.Start(new ProcessStartInfo { FileName = e.Href, UseShellExecute = true });
-            }
-        }
-
-        [RelayCommand]
-        private void EditPagefile()
-        {
-            Process.Start("SystemPropertiesAdvanced.exe");
         }
     }
 }
