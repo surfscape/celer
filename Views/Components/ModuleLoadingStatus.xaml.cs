f9e3417df635c060863a7b88c1969a1f01682045
ReduxFlakes <tiagompca@proton.me>
2026-01-02 01:43:34 +0000

    feat: new UserControl to show the loading state of heavy modules

---
+++ Views/Components/ModuleLoadingStatus.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Celer.Views.Components
+{
+    /// <summary>
+    /// Interaction logic for ModuleLoadingStatus.xaml
+    /// </summary>
+    public partial class ModuleLoadingStatus : UserControl
+    {
+        public ModuleLoadingStatus()
+        {
+            InitializeComponent();
+        }
+    }
+}
