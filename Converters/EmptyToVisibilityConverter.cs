2a168053328b645e65148b53bf81d5651d1089dc
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2026-03-31 18:16:46 +0100

    refactor: add converter for TabControl collapse and remove unused imports for converters

---
+++ Converters/EmptyToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Celer.Converters
+{
+    internal sealed class EmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return string.IsNullOrWhiteSpace(str) ? Visibility.Collapsed : Visibility.Visible;
+            }
+
+            return value is null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Converters/MenuStateToolTipConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Celer.Converters
+{
+    public class MenuStateToolTipConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture
+        ) => (bool)value ? "Open Navigation" : "Close Navigation";
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
