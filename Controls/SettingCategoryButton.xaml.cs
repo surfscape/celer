4120f748dab40ebddc46e753acd79e74983c12b7
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2026-03-31 18:15:35 +0100

    feat: add SettingCategory control

---
+++ Controls/SettingCategoryButton.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MahApps.Metro.IconPacks;
+using Metalama.Patterns.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Celer.Controls
+{
+    /// <summary>
+    /// Interaction logic for SettingCategoryButton.xaml
+    /// </summary>
+    public partial class SettingCategoryButton : UserControl
+    {
+        [DependencyProperty]
+        public PackIconLucideKind Icon { get; set; } = PackIconLucideKind.Bolt;
+
+        [DependencyProperty]
+        public string Title { get; set; } = "Option title";
+
+        [DependencyProperty]
+        public string Description { get; set; } = "Category description";
+
+        [DependencyProperty]
+        public ICommand? Command { get; set; }
+
+        [DependencyProperty]
+        public string? CommandParameter { get; set; }
+        public SettingCategoryButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
