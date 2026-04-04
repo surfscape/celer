c1ff3cd2a6a51efa3cd135be50ca89348bcee4eb
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2026-04-05 00:02:46 +0100

    refactor(perf): start gathering data only when the view has been loaded

--- Views/UserControls/MainApp/OptimizationViews/MemoryManagement.xaml.cs
+++ Views/UserControls/MainApp/OptimizationViews/MemoryManagement.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Celer.ViewModels.OptimizationVM;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Celer.Views.UserControls.MainApp.OptimizationViews
@@ -8,10 +9,20 @@ namespace Celer.Views.UserControls.MainApp.OptimizationViews
     /// </summary>
     public partial class MemoryManagement : UserControl
     {
+        private readonly MemoryViewModel _viewModel;
         public MemoryManagement(MemoryViewModel memoryViewModel)
         {
             InitializeComponent();
-            DataContext = memoryViewModel;
+            _viewModel = memoryViewModel;
+            DataContext = _viewModel;
+            Loaded += Memory_Loaded;
         }
+        private async void Memory_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Yield();
+            if (_viewModel.IsLoading)
+                await _viewModel.Initialize();
+        }
+
     }
 }
