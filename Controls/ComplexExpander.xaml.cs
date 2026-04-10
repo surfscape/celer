ebd74c4baab5e9590f0bd6d2ab6c3b69c61f0793
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2026-04-10 23:31:27 +0100

    refactor: add new control and tweaked SettingCategoryButton

---
+++ Controls/ComplexExpander.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MahApps.Metro.IconPacks;
+using Metalama.Patterns.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Celer.Controls
+{
+    /// <summary>
+    /// Interaction logic for ComplexExpander.xaml
+    /// </summary>
+    [ContentProperty(nameof(ExpanderContent))]
+    public partial class ComplexExpander : UserControl
+    {
+        [DependencyProperty]
+        public PackIconLucideKind Icon { get; set; } = PackIconLucideKind.Paperclip;
+
+        [DependencyProperty]
+        public string Title { get; set; } = "Expander title";
+
+
+        [DependencyProperty]
+        public string Description { get; set; } = "Expander description";
+
+        [DependencyProperty]
+        public object ExpanderContent { get; set; }
+
+        public ComplexExpander()
+        {
+            InitializeComponent();
+        }
+    }
+}
