247da8964d8fc0d3d9d3e7375e49e43e116a79cd
ReduxFlakes <reduxflakes@disroot.org>
2026-07-06 00:19:35 +0100

    refactor: add isLoading property to module base view model

--- ViewModels/BaseModuleViewModel.cs
+++ ViewModels/BaseModuleViewModel.cs
@@ -9,6 +9,12 @@ namespace Celer.ViewModels
         /// Property to define the max width of a module (either auto to fill the window or limit to the default 1100px)
         /// </summary>
         [ObservableProperty]
-        private string apperanceUserViewport = MainConfiguration.Default.AppearanceMaxWidth ? "960" : "auto";
+        public partial string ApperanceUserViewport { get; set; } = MainConfiguration.Default.AppearanceMaxWidth ? "960" : "auto";
+
+        /// <summary>
+        /// Property that tracks the current loading progress of the module's view module
+        /// </summary>
+        [ObservableProperty]
+        public partial bool IsLoading { get; set; } = true;
     }
 }
