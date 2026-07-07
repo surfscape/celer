64fc1b7c4179b57d58c62c7ff5f1b67a901300d7
ReduxFlakes <reduxflakes@disroot.org>
2026-07-07 02:00:08 +0100

    feat: advanced settings

--- Views/Pages/Settings/SettingsAdvancedViewModel.cs
+++ Views/Pages/Settings/SettingsAdvancedViewModel.cs
@@ -1,26 +1,70 @@
 ﻿using ByteSizeLib;
+using Celer.Properties;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Reflection;
 using System.Windows.Threading;
 
 namespace Celer.Views.Pages.Settings
 {
     public partial class SettingsAdvancedViewModel : SettingsBaseViewModel
     {
-        [ObservableProperty]
-        private string processMemory;
-
         private readonly DispatcherTimer _timer;
+
+        public static Dictionary<string, int> PollingOptions { get; } = new()
+        {
+            { "250ms", 250 },
+            { "500ms", 500 },
+            { "1000ms (Default)", 1000 },
+            { "1500ms", 1500 }
+        };
+
+        [ObservableProperty]
+        public partial KeyValuePair<string, int> PoolingRate { get; set; } = PollingOptions.FirstOrDefault(x => x.Value == MainConfiguration.Default.GeneralPollingRate);
+
+        partial void OnPoolingRateChanged(KeyValuePair<string, int> value)
+        {
+            MainConfiguration.Default.GeneralPollingRate = value.Value;
+            MainConfiguration.Default.Save();
+        }
+
+        public ObservableCollection<string> RenderingModes { get; } = ["Auto (Default)", "Hardware", "Software only" ];
+
+        [ObservableProperty]
+        public partial int GraphicRenderingMode { get; set; } = MainConfiguration.Default.GraphicRenderingMode;
+
+        partial void OnGraphicRenderingModeChanged(int value)
+        {
+            MainConfiguration.Default.GraphicRenderingMode = value;
+            MainConfiguration.Default.Save();
+        }
+
+        /* Debug Information Properties */
+        [ObservableProperty]
+        public partial string ProcessMemory { get; set; } = "0.00MB";
+
+        [ObservableProperty]
+        public partial string BuildType { get; set; } = "Unknown";
+
+
         public SettingsAdvancedViewModel()
         {
             _timer = new DispatcherTimer
             {
                 Interval = TimeSpan.FromSeconds(1)
             };
-            _timer.Tick += new EventHandler(updateDebugInfo);
+            _timer.Tick += (s, e) => UpdateDebugInfo();
             _timer.Start();
+            // Source - https://stackoverflow.com/a/60545278
+            // Posted by Egor Novikov
+            // Retrieved 2026-07-06, License - CC BY-SA 4.0
+            var assemblyConfigurationAttribute = typeof(SettingsAdvancedViewModel).Assembly.GetCustomAttribute<AssemblyConfigurationAttribute>();
+            if(assemblyConfigurationAttribute is not null && assemblyConfigurationAttribute.Configuration is not null)
+                BuildType = assemblyConfigurationAttribute.Configuration;
+
         }
 
-        private void updateDebugInfo(object sender, EventArgs e)
+        private void UpdateDebugInfo()
         {
             ProcessMemory = ByteSize.FromBytes(GC.GetTotalMemory(true)).ToString();
         }
