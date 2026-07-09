9ec5a5d8771171dcc2cedc27b966406503e38b0a
ReduxFlakes <reduxflakes@disroot.org>
2026-07-09 16:37:57 +0100

    refactor(perf): remove unused imports

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using Celer.Utilities;
 using Celer.ViewModels;
 using System.ComponentModel;
 using System.Windows;
-using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Shell;
 
