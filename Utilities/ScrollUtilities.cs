e005bec1f74bbc4973047d73728781796cd6f8b8
ReduxFlakes <reduxflakes@disroot.org>
2026-07-31 00:06:56 +0100

    refactor(perf): improvements and optimizations to smooth scrolling

--- Utilities/ScrollUtilities.cs
+++ Utilities/ScrollUtilities.cs
@@ -1,176 +1,118 @@
-﻿using System.Windows;
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
-namespace Celer.Utilities
+namespace Celer.Utilities;
+
+/// <summary>
+/// Class that provides both bubble event support and smooth scrolling
+/// </summary>
+// Most of this class was made with information from https://stackoverflow.com/questions/1033841/is-it-possible-to-implement-smooth-scroll-in-a-wpf-listview and in regards to bubble event that was taken from https://stackoverflow.com/questions/14348517/child-elements-of-scrollviewer-preventing-scrolling-with-mouse-wheel
+// There was some help from an local LLM to improve the scroll detection between mouse and touchpad since I'm not that great with math and physics 
+public static class ScrollUtilities
 {
-    /// <summary>
-    /// Class that provides both bubble event support and smooth scrolling
-    /// </summary>
-    // Most of this class was made with information from https://stackoverflow.com/questions/1033841/is-it-possible-to-implement-smooth-scroll-in-a-wpf-listview and in regards to bubble event that was taken from https://stackoverflow.com/questions/14348517/child-elements-of-scrollviewer-preventing-scrolling-with-mouse-wheel
-    // There was some help from an local LLM to improve the scroll detection between mouse and touchpad since I'm not that great with math and physics
-    public static class ScrollUtilities
+    public static readonly DependencyProperty SmoothScrollProperty =
+        DependencyProperty.RegisterAttached("SmoothScroll", typeof(bool), typeof(ScrollUtilities), new PropertyMetadata(false, OnSmoothScrollChanged));
+
+    public static bool GetSmoothScroll(DependencyObject obj) => (bool)obj.GetValue(SmoothScrollProperty);
+    public static void SetSmoothScroll(DependencyObject obj, bool value) => obj.SetValue(SmoothScrollProperty, value);
+
+    private static readonly ConditionalWeakTable<ScrollViewer, ScrollBehavior> _behaviors = new();
+
+    private static void OnSmoothScrollChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        public static readonly DependencyProperty SmoothScrollProperty =
-            DependencyProperty.RegisterAttached(
-                "SmoothScroll",
-                typeof(bool),
-                typeof(ScrollUtilities),
-                new PropertyMetadata(false, OnSmoothScrollChanged));
-
-        public static bool GetSmoothScroll(DependencyObject obj) => (bool)obj.GetValue(SmoothScrollProperty);
-        public static void SetSmoothScroll(DependencyObject obj, bool value) => obj.SetValue(SmoothScrollProperty, value);
-
-        private static readonly DependencyProperty TargetVerticalOffsetProperty =
-            DependencyProperty.RegisterAttached(
-                "TargetVerticalOffset",
-                typeof(double),
-                typeof(ScrollUtilities),
-                new PropertyMetadata(0.0));
-
-        private static readonly DependencyProperty IsAnimatingProperty =
-            DependencyProperty.RegisterAttached(
-                "IsAnimating",
-                typeof(bool),
-                typeof(ScrollUtilities),
-                new PropertyMetadata(false));
-
-        private static readonly DependencyProperty RenderingHandlerProperty =
-            DependencyProperty.RegisterAttached(
-                "RenderingHandler",
-                typeof(EventHandler),
-                typeof(ScrollUtilities));
-
-        private static void OnSmoothScrollChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        if (d is ScrollViewer sv)
         {
-            if (d is ScrollViewer scrollViewer)
-            {
-                if ((bool)e.NewValue)
-                {
-                    scrollViewer.PreviewMouseWheel += ScrollViewer_PreviewMouseWheel;
-                    scrollViewer.Loaded += ScrollViewer_Loaded;
-                    scrollViewer.Unloaded += ScrollViewer_Unloaded;
-                }
-                else
-                {
-                    scrollViewer.PreviewMouseWheel -= ScrollViewer_PreviewMouseWheel;
-                    scrollViewer.Loaded -= ScrollViewer_Loaded;
-                    scrollViewer.Unloaded -= ScrollViewer_Unloaded;
-                    StopAnimating(scrollViewer);
-                }
-            }
+            if ((bool)e.NewValue)
+                _behaviors.GetValue(sv, key => new ScrollBehavior(key)).Enable();
+            else if (_behaviors.TryGetValue(sv, out var behavior))
+                behavior.Disable();
         }
+    }
+}
 
-        private static void ScrollViewer_Loaded(object sender, RoutedEventArgs e)
+internal class ScrollBehavior(ScrollViewer sv)
+{
+    private readonly ScrollViewer _sv = sv;
+    private double _targetOffset;
+    private bool _isAnimating;
+    private TimeSpan _lastRenderTime;
+
+    public void Enable()
+    {
+        _sv.PreviewMouseWheel += OnMouseWheel;
+        _sv.Loaded += OnLoaded;
+        _sv.Unloaded += OnUnloaded;
+    }
+
+    public void Disable()
+    {
+        _sv.PreviewMouseWheel -= OnMouseWheel;
+        _sv.Loaded -= OnLoaded;
+        _sv.Unloaded -= OnUnloaded;
+        StopAnimation();
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e) => _targetOffset = _sv.VerticalOffset;
+
+    private void OnUnloaded(object sender, RoutedEventArgs e) => StopAnimation();
+
+    private void OnMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        if (e.Handled) return;
+
+        double current = _sv.VerticalOffset;
+
+        if ((e.Delta > 0 && _targetOffset > current) || (e.Delta < 0 && _targetOffset < current))
+            _targetOffset = current;
+
+        double scrollAmount = -e.Delta * 0.5;
+        _targetOffset = Math.Clamp(_targetOffset + scrollAmount, 0, _sv.ScrollableHeight);
+
+        if ((current <= 0 && scrollAmount < 0) || (current >= _sv.ScrollableHeight && scrollAmount > 0))
+            return;
+
+        e.Handled = true;
+
+        if (e.Delta % 120 != 0)
         {
-            if (sender is ScrollViewer scrollViewer)
-            {
-                scrollViewer.SetValue(TargetVerticalOffsetProperty, scrollViewer.VerticalOffset);
-            }
+            _sv.ScrollToVerticalOffset(_targetOffset);
         }
-
-        private static void ScrollViewer_Unloaded(object sender, RoutedEventArgs e)
+        else if (!_isAnimating)
         {
-            if (sender is ScrollViewer scrollViewer)
-            {
-                StopAnimating(scrollViewer);
-            }
+            _isAnimating = true;
+            _lastRenderTime = TimeSpan.Zero;
+            CompositionTarget.Rendering += OnRender;
         }
+    }
 
-        private static void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    private void OnRender(object? sender, EventArgs e)
+    {
+        if (e is not RenderingEventArgs args) return;
+
+        double dt = _lastRenderTime == TimeSpan.Zero ? 0.016 : (args.RenderingTime - _lastRenderTime).TotalSeconds;
+        _lastRenderTime = args.RenderingTime;
+
+        double current = _sv.VerticalOffset;
+        double step = (_targetOffset - current) * (1.0 - Math.Exp(-12.0 * dt));
+
+        if (Math.Abs(_targetOffset - current) < 1.0)
         {
-            if (sender is not ScrollViewer scrollViewer || e.Handled)
-                return;
-
-            bool isTouchpad = (e.Delta % 120 != 0);
-
-            double currentOffset = scrollViewer.VerticalOffset;
-            double targetOffset = (double)scrollViewer.GetValue(TargetVerticalOffsetProperty);
-
-            if ((e.Delta > 0 && targetOffset > currentOffset) || (e.Delta < 0 && targetOffset < currentOffset))
-            {
-                targetOffset = currentOffset;
-            }
-
-            double multiplier = 0.5;
-            double scrollAmount = -e.Delta * multiplier;
-
-            double newTarget = targetOffset + scrollAmount;
-            newTarget = Math.Clamp(newTarget, 0, scrollViewer.ScrollableHeight);
-
-            bool atTop = currentOffset <= 0 && scrollAmount < 0;
-            bool atBottom = currentOffset >= scrollViewer.ScrollableHeight && scrollAmount > 0;
-
-            if (atTop || atBottom)
-            {
-                return;
-            }
-
-            e.Handled = true;
-            scrollViewer.SetValue(TargetVerticalOffsetProperty, newTarget);
-
-            if (isTouchpad)
-            {
-                scrollViewer.ScrollToVerticalOffset(newTarget);
-            }
-            else
-            {
-                if (!(bool)scrollViewer.GetValue(IsAnimatingProperty))
-                {
-                    scrollViewer.SetValue(IsAnimatingProperty, true);
-                    CompositionTarget.Rendering += GetRenderingHandler(scrollViewer);
-                }
-            }
+            _sv.ScrollToVerticalOffset(_targetOffset);
+            StopAnimation();
         }
-
-        private static EventHandler GetRenderingHandler(ScrollViewer scrollViewer)
+        else
         {
-            if (scrollViewer.GetValue(RenderingHandlerProperty) is not EventHandler handler)
-            {
-                TimeSpan lastRenderTime = TimeSpan.Zero;
-
-                handler = (sender, args) =>
-                {
-                    var renderingArgs = (RenderingEventArgs)args;
-
-                    double deltaTime = (renderingArgs.RenderingTime - lastRenderTime).TotalSeconds;
-                    if (lastRenderTime == TimeSpan.Zero) deltaTime = 0.016;
-                    lastRenderTime = renderingArgs.RenderingTime;
-
-                    double current = scrollViewer.VerticalOffset;
-                    double target = (double)scrollViewer.GetValue(TargetVerticalOffsetProperty);
-
-                    double speed = 12.0;
-                    double factor = 1.0 - Math.Exp(-speed * deltaTime);
-                    double step = (target - current) * factor;
-
-                    if (Math.Abs(target - current) < 1.0)
-                    {
-                        scrollViewer.ScrollToVerticalOffset(target);
-                        StopAnimating(scrollViewer);
-                        lastRenderTime = TimeSpan.Zero;
-                    }
-                    else
-                    {
-                        scrollViewer.ScrollToVerticalOffset(current + step);
-                    }
-                };
-                scrollViewer.SetValue(RenderingHandlerProperty, handler);
-            }
-            return handler;
+            _sv.ScrollToVerticalOffset(current + step);
         }
+    }
 
-        private static void StopAnimating(ScrollViewer scrollViewer)
-        {
-            if ((bool)scrollViewer.GetValue(IsAnimatingProperty))
-            {
-                scrollViewer.SetValue(IsAnimatingProperty, false);
-                if (scrollViewer.GetValue(RenderingHandlerProperty) is EventHandler handler)
-                {
-                    CompositionTarget.Rendering -= handler;
-                }
-            }
-        }
+    private void StopAnimation()
+    {
+        if (!_isAnimating) return;
+        _isAnimating = false;
+        CompositionTarget.Rendering -= OnRender;
     }
 }
