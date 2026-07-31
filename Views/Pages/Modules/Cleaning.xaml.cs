6ac76d29d8a3765143ecd80f211cc91a3042e793
ReduxFlakes <reduxflakes@disroot.org>
2026-07-31 21:00:50 +0100

    feat: prevent autoscroll when scrolled to opposite direction and fully implement virtualization

--- Views/Pages/Modules/Cleaning.xaml.cs
+++ Views/Pages/Modules/Cleaning.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Celer.ViewModels;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace Celer.Views.Pages.Modules
@@ -8,20 +8,41 @@ namespace Celer.Views.Pages.Modules
     /// </summary>
     public partial class Cleaning : UserControl
     {
-
+        private bool autoScrollPaused = false;
         public Cleaning()
         {
             InitializeComponent();
             LogListBox.Items.MoveCurrentToLast();
             LogListBox.ScrollIntoView(LogListBox.Items.CurrentItem);
+            Loaded += (s, e) =>
+            {
+                LogListBox.SetValue(VirtualizingStackPanel.VirtualizationModeProperty, VirtualizationMode.Recycling);
+                LogListBox.Visibility = Visibility.Visible;
+            };
         }
 
         private void ScrollViewer_OnScrollChanged(object sender, ScrollChangedEventArgs e)
         {
-            if (e.OriginalSource is ScrollViewer scrollViewer &&
-                Math.Abs(e.ExtentHeightChange) > 0.0)
+            if(!autoScrollPaused)
+                if (e.OriginalSource is ScrollViewer scrollViewer &&
+                    Math.Abs(e.ExtentHeightChange) > 0.0)
+                {
+                    scrollViewer.ScrollToBottom();
+                }
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (e.OriginalSource is ScrollViewer scrollViewer)
             {
-                scrollViewer.ScrollToBottom();
+                if (scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight)
+                {
+                    autoScrollPaused = false;
+                }
+                else
+                {
+                    autoScrollPaused = true;
+                }
             }
         }
 
